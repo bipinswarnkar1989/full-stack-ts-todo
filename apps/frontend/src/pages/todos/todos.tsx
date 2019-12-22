c79126ff2508af3a5e2f6deb8cb2a4064f8dac19
Bipin Swarnkar <bipin.swarnkar@theagilehub.net>
2019-12-22 22:32:08 +0530

    feat: add todos listing

--- apps/frontend/src/pages/todos/todos.tsx
+++ apps/frontend/src/pages/todos/todos.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import * as Styled from './styles';
+import { GetTodosQuery as GetTodosProps } from './todos.gql.generated';
 
-export const Todos: React.FC = () => {
+export const Todos: React.FC<GetTodosProps> = ({ todos }) => {
     return (
       <Styled.Container>
         <Styled.Main>
         <Styled.Title color="#3D7E9A">List Todos</Styled.Title>
+        <Styled.Todos>
+        <ul>
+         {todos && todos.map(todo => 
+            <li>{todo?.text}</li>
+          )}
+         </ul>
+        </Styled.Todos>
         </Styled.Main>
       </Styled.Container>
     )
